a303108fa6caa8ea8404f89c81a9bf78613e9d2f
jiangxiaolong <379352157@qq.com>
2026-05-07 17:48:06 +0800

    feat(home): 添加草稿页面原生广告和可见性检测功能
    - 在 DraftController 中重构广告加载逻辑，添加 loadNVhomeAd 方法
    - 实现页面可见性检测，当页面可见时自动加载原生广告
    - 集成 visibility_detector 包用于监听页面可见性变化
    - 更新页面生命周期方法，添加 handRegister 和 fetchData 方法
    - 修复广告加载时机问题，优化广告显示逻辑
    - 添加依赖包 visibility_detector ^0.4.0+2

--- lib/modules/home/controllers/draft_controller.dart
+++ lib/modules/home/controllers/draft_controller.dart
@@ -13,28 +13,10 @@ class DraftController extends BaseController {
   /// draft列表
   var draftList = <MemoryInfo>[];
 
+  final scenario = 'NVhome';
+
   @override
-  void onInit() {
-    super.onInit();
-    _checkAndListenNVhomeAd();
-    getDataFromLocal();
-  }
-
-  void getDataFromLocal() async {
-    draftList = Storage.getDraftMemories();
-    draftList.sort((a, b) => (b.operationTime ?? 0).compareTo(a.operationTime ?? 0));
-    multiStatusType = draftList.isEmpty ? MultiStatusType.statusEmpty : MultiStatusType.statusContent;
-    update();
-  }
-
-  void deleteDraft(MemoryInfo memoryInfo) async {
-    await Storage.deleteDraftMemory(memoryInfo.id ?? '');
-    getDataFromLocal();
-  }
-
-  void _checkAndListenNVhomeAd() {
-    final scenario = 'NVhome';
-
+  void handRegister() {
     // 设置回调监听，无论是现在还在加载中，还是以后重新加载，只要成功/失败都会触发
     NativeAdManager.instance.setListener(
       scenario,
@@ -50,21 +32,42 @@ class DraftController extends BaseController {
         update();
       },
     );
+  }
 
+  @override
+  void fetchData() {
+    getDataFromLocal();
+    loadNVhomeAd();
+  }
+
+  void getDataFromLocal() async {
+    draftList = Storage.getDraftMemories();
+    draftList.sort((a, b) => (b.operationTime ?? 0).compareTo(a.operationTime ?? 0));
+    multiStatusType = draftList.isEmpty ? MultiStatusType.statusEmpty : MultiStatusType.statusContent;
+    update();
+  }
+
+  void deleteDraft(MemoryInfo memoryInfo) async {
+    await Storage.deleteDraftMemory(memoryInfo.id ?? '');
+    getDataFromLocal();
+  }
+
+  /// 加载原生广告
+  void loadNVhomeAd() {
     // 处理时间差问题：如果在进入该页面时，LaunchController 中的加载【已经】失败了，
     // 原生管理器里现在是没有广告的，也没有在加载中的状态，我们需要主动重试。
     // 但是 NativeAdManager 目前没有暴露 `isCurrentlyLoading` 状态，
     // 最安全的做法是：如果当前没加载好，我们就主动发一次加载请求。
     // 如果它内部其实已经加载好了，isAdLoaded 会返回 true，那么就不需要重新加载，只需 update() 渲染即可。
-    if (NativeAdManager.instance.isAdLoaded(scenario)) {
-      update();
-    } else if (!NativeAdManager.instance.isAdLoading(scenario)) {
-      // 当前不可用且没有在加载中，为了保险起见重新发起请求
+    // if (NativeAdManager.instance.isAdLoaded(scenario)) {
+    //   update();
+    // } else if (!NativeAdManager.instance.isAdLoading(scenario)) {
+    //   // 当前不可用且没有在加载中，为了保险起见重新发起请求
       _retryLoadAd(scenario);
-    } else {
-      // 正在加载中，不做任何额外请求，等待 listener 触发回调即可
-      commonDebugPrint('SettingController: $scenario ad is currently loading. Waiting for callback.');
-    }
+    // } else {
+    //   正在加载中，不做任何额外请求，等待 listener 触发回调即可
+      // commonDebugPrint('SettingController: $scenario ad is currently loading. Waiting for callback.');
+    // }
   }
 
   void _retryLoadAd(String scenario) {
@@ -78,7 +81,7 @@ class DraftController extends BaseController {
   void onClose() {
     // 离开页面时，如果不需要保留该原生广告缓存，可以销毁
     // 如果想要全局保留给其他页面复用，则不要在这里 dispose
-    // NativeAdManager.instance.disposeAd('NVhome');
+    NativeAdManager.instance.disposeAd('NVhome');
     super.onClose();
   }
 }
--- lib/modules/home/views/draft_page.dart
+++ lib/modules/home/views/draft_page.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 class DraftPage extends StatefulWidget {
   const DraftPage({super.key});
@@ -24,55 +25,63 @@ class _DraftPageState extends State<DraftPage> with AutomaticKeepAliveClientMixi
     return GetBuilder<DraftController>(
       init: DraftController(),
       builder: (controller) {
-        return Column(
-          children: [
-            Expanded(
-              child: CommonRefresh.instance(
-                hasBefore: false,
-                hasMore: false,
-                child: MultiStatusView(
-                  hasAppBar: false,
-                  currentStatus: controller.multiStatusType,
-                  child: GridView.builder(
-                    padding: EdgeInsets.only(left: 8.w, top: 12.h, right: 8.w, bottom: NativeAdManager.instance.isAdLoaded('NVhome') && NativeAdManager.instance.getNativeAd('NVhome') != null ? 12.h : 50.h),
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      crossAxisSpacing: 15.w,
-                      mainAxisSpacing: 15.w,
-                      childAspectRatio: 164 / 150,
+        return VisibilityDetector(
+          key: const Key('draftPage'),
+          onVisibilityChanged: (info) async {
+            if (info.visibleFraction > 0.0) {
+              controller.loadNVhomeAd();
+            }
+          },
+          child: Column(
+              children: [
+                Expanded(
+                  child: CommonRefresh.instance(
+                    hasBefore: false,
+                    hasMore: false,
+                    child: MultiStatusView(
+                      hasAppBar: false,
+                      currentStatus: controller.multiStatusType,
+                      child: GridView.builder(
+                        padding: EdgeInsets.only(left: 8.w, top: 12.h, right: 8.w, bottom: NativeAdManager.instance.isAdLoaded('NVhome') && NativeAdManager.instance.getNativeAd('NVhome') != null ? 12.h : 50.h),
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 15.w,
+                          mainAxisSpacing: 15.w,
+                          childAspectRatio: 164 / 150,
+                        ),
+                        itemCount: controller.draftList.length,
+                        itemBuilder: (context, index) {
+                          final memoryInfo = controller.draftList[index];
+                          return VideoCell(
+                            memoryInfo: memoryInfo,
+                            cellType: VideoCellType.draft,
+                            videoAction: (memoryInfo, cellType) {
+                              Get.toNamed(Routes.editVideo, arguments: {'memoryInfo': memoryInfo, 'isFromDraft': true});
+                            },
+                            operationAction: (memoryInfo, cellType) {
+                              controller.deleteDraft(memoryInfo);
+                            },
+                          );
+                        },
+                      ),
                     ),
-                    itemCount: controller.draftList.length,
-                    itemBuilder: (context, index) {
-                      final memoryInfo = controller.draftList[index];
-                      return VideoCell(
-                        memoryInfo: memoryInfo,
-                        cellType: VideoCellType.draft,
-                        videoAction: (memoryInfo, cellType) {
-                          Get.toNamed(Routes.editVideo, arguments: {'memoryInfo': memoryInfo, 'isFromDraft': true});
-                        },
-                        operationAction: (memoryInfo, cellType) {
-                          controller.deleteDraft(memoryInfo);
-                        },
-                      );
-                    },
                   ),
                 ),
-              ),
-            ),
-            // 原生广告展示区域
-            if (NativeAdManager.instance.isAdLoaded('NVhome') && NativeAdManager.instance.getNativeAd('NVhome') != null)
-              Container(
-                width: 300,
-                height: 250,
-                margin: EdgeInsets.only(top: 12.h, bottom: 30.h),
-                decoration: BoxDecoration(
-                  color: CommonColors.color333333,
-                  borderRadius: BorderRadius.circular(16.r),
-                ),
-                clipBehavior: Clip.antiAlias,
-                child: AdWidget(ad: NativeAdManager.instance.getNativeAd('NVhome')!),
-              ),
-          ]
+                // 原生广告展示区域
+                if (NativeAdManager.instance.isAdLoaded('NVhome') && NativeAdManager.instance.getNativeAd('NVhome') != null)
+                  Container(
+                    width: 300,
+                    height: 250,
+                    margin: EdgeInsets.only(top: 12.h, bottom: 30.h),
+                    decoration: BoxDecoration(
+                      color: CommonColors.color333333,
+                      borderRadius: BorderRadius.circular(16.r),
+                    ),
+                    clipBehavior: Clip.antiAlias,
+                    child: AdWidget(ad: NativeAdManager.instance.getNativeAd('NVhome')!),
+                  ),
+              ]
+          ),
         );
       },
     );
